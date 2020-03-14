3c925971afa177ef7fbd10e5c9b0891befac6ec4
igorpieniek <pieniek97@gmail.com>
2020-03-14 20:19:31 +0100

    Tkinter added - v1 finished

--- sudoku_solver/CellTkinter.py
+++ sudoku_solver/CellTkinter.py
@@ -1,18 +1,21 @@
-from tkinter import Entry
+from tkinter import *
+
 class CellTkinter(object):
     def __init__(self, root,row,column):
         self._root = root
         self._row = row
         self._column = column
+        self._addEntry()
 
     def _addEntry(self):
-        self._value = ( Entry(self.nameFrame, width=5 ,textvariable = IntVar()) )
-        self._value.grid( row= self._row , column=self._column ,padx = 0, pady=0, sticky = N+W+E)
+        self._value = ( Entry(self._root, width=4 ,textvariable = IntVar()) )
+        self._value.grid( row= self._row , column=self._column ,padx = 3, pady=3, sticky = N+W+E)
     
     def getValue(self):
-        return self._value
+        return self._value.get()
 
-    def setValue(self, val):
+    def setValue(self, val): 
+        if not self.getValue() : self._value.config(background = "red")
         self._value.delete(0,END)
-        self._value.insert(0,val)
-        self._root.config(backgrount = "red")
+        self._value.insert(0,str(val))
+        
--- sudoku_solver/sudoku_solver.py
+++ sudoku_solver/sudoku_solver.py
@@ -1,17 +1,60 @@
 from Solver import *
+from tkinter import *
+from CellTkinter import CellTkinter
 
+root = Tk()
+root.geometry('370x315')
 solv = Solver()
 
-array =[[0 ,0 ,9 ,  0 ,8 ,0 ,   5 ,0 ,0 ],
-        [0 ,1 ,0 ,  0 ,0 ,0 ,   0 ,0 ,2 ],   
-        [5 ,0 ,0 ,  4 ,3 ,0 ,   8 ,0 ,6 ],
+def solve(array):
+    return solv.solve(array)
 
-        [9 ,0 ,6 ,  0 ,0 ,5 ,   0 ,0 ,3 ],
-        [0 ,5 ,0 ,  0 ,7 ,0 ,   0 ,0 ,0 ],
-        [0 ,4 ,0 ,  0 ,0 ,8 ,   1 ,0 ,9 ],
+def sendArray(array):
+    #build multi dimension array
+    arrToSend = []
+    rows = []
+    
+    for c in range(len(array[0])):
+        rows =[]
+        for r in range(len(array[0])):
+            rows.append(int(array[r][c].getValue()))
+        arrToSend.append(rows)
 
-        [0 ,0 ,1 ,  3 ,0 ,0 ,   0 ,0 ,4 ],
-        [0 ,0 ,4 ,  0 ,0 ,9 ,   6 ,0 ,0 ],
-        [8 ,3 ,0 ,  0 ,6 ,0 ,   0 ,1 ,0 ]]
+    solved = []
+    solved = solve(arrToSend)
+     
+    for c in range(len(array[0])):
+        for r in range(len(array[0])):
+            array[c][r].setValue(solved[r][c])
 
-solv.solve(array)
+
+buttonFrame = LabelFrame(root, padx = 5, pady=5,width =300)
+buttonFrame .grid(row = 0,column = 0, stick = W+N+E)
+entryFrame = LabelFrame(root, padx = 5, pady=5)
+entryFrame .grid(row = 1,column = 0, stick = W+N+E)
+button = Button(buttonFrame, text= "Solve",padx=0, pady=0, command = lambda:sendArray(array))
+button.grid(row = 0,column = 0, stick = W+N+E)
+
+array = []
+for c in range(9):
+    col = []
+    for r in range(9):
+        col.append(CellTkinter(entryFrame,r,c))        
+    array.append(col)
+
+#array =[[0 ,0 ,9 ,  0 ,8 ,0 ,   5 ,0 ,0 ],
+#        [0 ,1 ,0 ,  0 ,0 ,0 ,   0 ,0 ,2 ],   
+#        [5 ,0 ,0 ,  4 ,3 ,0 ,   8 ,0 ,6 ],
+
+#        [9 ,0 ,6 ,  0 ,0 ,5 ,   0 ,0 ,3 ],
+#        [0 ,5 ,0 ,  0 ,7 ,0 ,   0 ,0 ,0 ],
+#        [0 ,4 ,0 ,  0 ,0 ,8 ,   1 ,0 ,9 ],
+
+#        [0 ,0 ,1 ,  3 ,0 ,0 ,   0 ,0 ,4 ],
+#        [0 ,0 ,4 ,  0 ,0 ,9 ,   6 ,0 ,0 ],
+#        [8 ,3 ,0 ,  0 ,6 ,0 ,   0 ,1 ,0 ]]
+
+#solv.solve(array)
+
+
+root.mainloop()
