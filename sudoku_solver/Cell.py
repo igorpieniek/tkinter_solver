9750c36656074e35521d401768cb61f36a449c4e
igorpieniek <pieniek97@gmail.com>
2020-03-14 16:44:23 +0100

    Add calculate area function in cell class

--- sudoku_solver/Cell.py
+++ sudoku_solver/Cell.py
@@ -4,6 +4,7 @@ class Cell(object):
         self._column = column
         self._options = [x for x in range(1,10)]
         self._value =0
+        self._calculateArea()
 
     def getRow(self):
         return self._row
@@ -20,12 +21,47 @@ class Cell(object):
     def getNumOfOpt(self):
         return len(self._options)
 
+    def getAreaRowMin(self):
+        return self._areaRowMin
+    def getAreaRowMax(self):
+        return self._areaRowMax
+    def getAreaColumnMin(self):
+        return self._areaColumnMin
+    def getAreaColumnMax(self):
+        return self._areaColumnMax
+
     def getValue(self):
         if len(self._options)==1:
             self._value =self._options[0]
             return self._value
         else:
             return self._value
+    
+    def _setBoundariesOfArea(self, rowMin, rowMax, columnMin, columnMax):
+            self._areaRowMin = rowMin
+            self._areaRowMax = rowMax
+            self._areaColumnMin = columnMin
+            self._areaColumnMax = columnMax
 
+    def _calculateArea(self):
+        if  self._row < 3 and self._column < 3 :
+            self._setBoundariesOfArea(0,3,0,3)
+        elif  self._row < 3 and self._column > 2 and self._column < 6 :
+            self._setBoundariesOfArea(0,3,3,6)
+        elif  self._row < 3 and self._column > 5 :
+            self._setBoundariesOfArea(0,3,6,9)
 
+        elif self._row > 2 and self._row < 6 and self._column < 3 :
+            self._setBoundariesOfArea(3,6,0,3)
+        elif  self._row > 2 and self._row < 6 and self._column > 2 and self._column < 6 :
+            self._setBoundariesOfArea(3,6,3,6 )
+        elif  self._row > 2 and self._row < 6  and self._column > 5 :
+            self._setBoundariesOfArea(3,6,6,9)
+
+        elif  self._row > 5 and self._column < 3 :
+            self._setBoundariesOfArea(6,9,0,3)
+        elif  self._row > 5 and self._column > 2 and self._column < 6 :
+            self._setBoundariesOfArea(6,9,3,6)
+        elif  self._row > 5 and self._column > 5 :
+            self._setBoundariesOfArea(6,9,6,9)
 
