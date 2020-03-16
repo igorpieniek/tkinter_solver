a40431967a1f10c4c779d5b4f3af440bc3d35fe6
igorpieniek <pieniek97@gmail.com>
2020-03-16 11:43:42 +0100

    Add clear button

--- sudoku_solver/sudoku_solver.py
+++ sudoku_solver/sudoku_solver.py
@@ -5,6 +5,7 @@ from CellTkinter import CellTkinter
 root = Tk()
 root.geometry('370x315')
 solv = Solver()
+array = []
 
 def solve(array):
     return solv.solve(array)
@@ -27,6 +28,13 @@ def sendArray(array):
         for r in range(len(array[0])):
             array[c][r].setValue(solved[r][c])
 
+def clear():
+    array = []
+    for c in range(9):
+        col = []
+        for r in range(9):
+            col.append(CellTkinter(entryFrame,r,c))        
+        array.append(col)
 
 buttonFrame = LabelFrame(root, padx = 5, pady=5,width =300)
 buttonFrame .grid(row = 0,column = 0, stick = W+N+E)
@@ -34,8 +42,10 @@ entryFrame = LabelFrame(root, padx = 5, pady=5)
 entryFrame .grid(row = 1,column = 0, stick = W+N+E)
 button = Button(buttonFrame, text= "Solve",padx=0, pady=0, command = lambda:sendArray(array))
 button.grid(row = 0,column = 0, stick = W+N+E)
+buttonClear = Button(buttonFrame, text= "Clear",padx=0, pady=0, command = lambda:clear())
+buttonClear.grid(row = 0,column = 1, stick = W+N+E)
 
-array = []
+
 for c in range(9):
     col = []
     for r in range(9):
